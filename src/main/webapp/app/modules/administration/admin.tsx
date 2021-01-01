e0bf73ab1ce661bf042ad818896530826195fc77
Wube <wubshet.kifle@gsa.gov>
2020-12-31 20:28:01 -0500

    updated admin component

--- src/main/webapp/app/modules/administration/admin.tsx
+++ src/main/webapp/app/modules/administration/admin.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
-const Admin = () => (
-  <div>
-    <p className="usa-text-large">Admin Page</p>
-  </div>
-);
+import React, { useState, useEffect } from 'react';
+export const Admin = () => {
+  useEffect(() => {
+    document.title = 'Admin';
+  }, []);
+  return (
+      <div>
+        <p className="usa-text-large">Admin Page</p>
+      </div>
+    );
+};
 export default Admin;
